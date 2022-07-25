a97b3dde76d719c7657d47818fb164f362e0c76a
Daniil <57467826+danredtmf@users.noreply.github.com>
2022-07-25 23:41:08 +0700

    Этап 1: 0.8.3-alpha
    - эффекты из фона главного меню теперь разделены
    - добавлено предупреждение в заставке
    - добавлен перевод

--- scenes/splash_screen/splash_screen.gd
+++ scenes/splash_screen/splash_screen.gd
@@ -1,19 +1,43 @@
 extends Control
 
-func _ready():
-	_start_animation()
+var is_warning_showed := false
 
-func _start_animation():
-	$Anim.play("show")
+func _ready() -> void:
+	$VB2/Margin/VBWarning/Header.text = tr('warning')
+	$VB2/Margin/VBWarning/Info.text = tr('warning_info')
+	
+	$VB3/Margin/Info.text = tr('splash_hint')
+	
+	_show_dev_animation()
 
-func _end_animation():
-	$Anim.play("hide")
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed('ui_accept'):
+		if is_warning_showed:
+			_hide_warning_animation()
 
-func _on_Anim_animation_finished(anim_name):
-	if anim_name == "show":
+func _show_dev_animation() -> void:
+	$Animation.play("show_dev")
+
+func _hide_dev_animation() -> void:
+	$Animation.play("hide_dev")
+
+func _show_warning_animation() -> void:
+	$Animation.play('show_warning')
+
+func _hide_warning_animation() -> void:
+	is_warning_showed = false
+	$Animation.play('hide_warning')
+
+func _on_Animation_finished(anim_name: String) -> void:
+	if anim_name == "show_dev":
 		yield(get_tree().create_timer(1), "timeout")
-		_end_animation()
-	elif anim_name == "hide":
+		_hide_dev_animation()
+	elif anim_name == "hide_dev":
+		yield(get_tree().create_timer(1), "timeout")
+		_show_warning_animation()
+	elif anim_name == "show_warning":
+		is_warning_showed = true
+	elif anim_name == "hide_warning":
 		Data.splash_screen_is_appeared = true
 		yield(get_tree().create_timer(1), "timeout")
 		Core.load_scene("main_menu", Core.main_menu_res)
